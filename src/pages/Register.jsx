50ee55f28ebb1f57c8266ac06050beb05000f832
Ronnie Alemany Pineda <ralemany@uci.edu>
2021-02-01 23:26:57 -0800

    register completed

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,36 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Idm from "../services/Idm";
 import "../css/form.css";
 
 const Register = () => {
-    return (
-      <div className="form-box">
-        <h1>Register</h1>
-      </div>
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const handleRegister = (e) =>  {
+    e.preventDefault();
+
+    Idm.register(email, password)
+      .then(response => alert(JSON.stringify(response.data, null, 4)))
+      .catch(error => alert(error));
+  }
+  return (
+    <div className="form-box">
+            <h1>Register</h1>
+            <form onSubmit={handleRegister}>
+                <label className="form-label">Email</label>
+                <input
+                    className="form-input"
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <label className="form-label">Password</label>
+                <input
+                    className="form-input"
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button className="form-button">Register</button>
+            </form>
+        </div>
     );
 }
 
